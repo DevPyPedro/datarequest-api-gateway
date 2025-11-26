05c0a748968046f0b42f9a7f0a09d8b1a60c1f19
DevPyPedro <dev.santiago.felicio@gmail.com>
2025-11-26 17:37:35 -0300

    Create interface for user repository

---
+++ app/domain/repositories/user_repository_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+class UserRepositoryInterface(ABC):
+    @abstractmethod
+    def get_user_by_id(self, user_id: int) -> Optional[dict]:
+        """Retrieve a user by their ID."""
+        pass
+
+    @abstractmethod
+    def create_user(self, user_data: dict) -> dict:
+        """Create a new user with the provided data."""
+        pass
--- app/infrastructure/repositories/user_repository.py
+++ app/infrastructure/repositories/user_repository.py
@@ -1,13 +1,22 @@
-from app.domain.entities.users import User
+import pandas as pd 
 
-class UserRepository:
+from app.domain.repositories.user_repository_interface import UserRepositoryInterface
+from app.domain.entities.users import User
+class UserRepository(UserRepositoryInterface):
     
     def __init__(self, db_session: any):
         self.db_session = db_session
 
     def get_user_by_id(self, user_id: int):
         """Logic to retrieve a user by their ID from the database"""
-        return self.db_session.query(User).filter(User.id == user_id).first()
+        try:
+            result = pd.read_sql(self.db_session.query(User).filter(User.userid == user_id).statement, self.db_session.bind)
+            print(result.to_dict(orient="records")[0])
+            if result.empty:
+                return None
+            return result.to_dict(orient="records")[0]
+        except Exception as e:
+            raise e
 
     def create_user(self, user_data):
         """Logic to create a new user in the database"""
